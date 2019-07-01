1d5a8dbbc41bfe90da8465e7bec0f41cbeb3a666
tokafew420 <tokafew420@gmail.com>
2019-07-01 06:11:24 -0400

    Collapsible movie summary

--- index.js
+++ index.js
@@ -12,18 +12,32 @@
         '&include_adult=false' +
         '&include_video=true' +
         '&page=1' +
-        '&primary_release_date.gte=2019-03-01', function (res) {
+        '&primary_release_date.gte=2019-03-01',
+        function (res) {
             if (res && res.results) {
                 var $row = $('.row', $movies);
                 res.results.forEach(function (movie) {
-                    var $card = $movieTemplate.clone().removeClass('movie-template d-none');
+                    var movieDescId = 'movie-desc-' + movie.id;
+                    var $card = $movieTemplate.clone().removeClass('movie-template d-none').addClass('movie');
                     $('.card-title', $card).text(movie.title);
-                    $('.card-text', $card).text(movie.overview);
                     $('.card-img-top', $card).attr('src', imageUrl300x450 + movie.poster_path).attr('alt', movie.title);
+                    $('#movie-desc', $card).attr('id', movieDescId)
+                    $('#' + movieDescId + ' .card-text', $card).text(movie.overview);
+                    $('.movie-desc-toggle', $card).attr('href', '#' + movieDescId).attr('aria-controls', movieDescId);
                     $row.append($card);
+
                 });
             }
         });
-        
+
     $('footer #year').text(new Date().getFullYear());
+
+    $('body').on('shown.bs.collapse', '.movie-desc', function () {
+        console.log(this);
+        var $this = $(this);
+        $('.movie-desc-toggle', $this.parent()).text('Show less');
+    }).on('hidden.bs.collapse', '.movie-desc', function () {
+        var $this = $(this);
+        $('.movie-desc-toggle', $this.parent()).text('Show more');
+    });
 })(window, jQuery);
